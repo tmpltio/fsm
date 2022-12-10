07e8d98fa2b3d3cecf5c29585c039f22f0cce346
Michał Maciejewski <tmplt@tmplt.io>
2022-12-10 18:16:47 +0100

    Transition factory

--- include/tmplt/fsm/fsm.h
+++ include/tmplt/fsm/fsm.h
@@ -8,6 +8,11 @@
 namespace tmplt::fsm
 {
 
+struct default_event_t
+{
+    explicit constexpr default_event_t() = default;
+};
+
 namespace detail
 {
 
@@ -62,6 +67,61 @@ concept transition_with_action = requires
     requires transition<Transition>;
     typename detail::action_t<Transition>;
     requires detail::transition_action<detail::action_t<Transition>, detail::event_t<Transition>>;
+};
+
+class transition_factory
+{
+    template<typename Event, typename Destination>
+    struct internal_transition
+    {
+        using event_t = Event;
+        using destination_t = Destination;
+    };
+
+    template<typename Transition, typename Guard>
+    struct internal_transition_with_guard : Transition
+    {
+        Guard guard;
+    };
+
+    template<typename Transition, typename Guard>
+    internal_transition_with_guard(Transition&&, Guard&&) -> internal_transition_with_guard<std::remove_cvref_t<Transition>, std::remove_cvref_t<Guard>>;
+
+    template<typename Transition, typename Action>
+    struct internal_transition_with_action : Transition
+    {
+        Action action;
+    };
+
+    template<typename Transition, typename Action>
+    internal_transition_with_action(Transition&&, Action&&) -> internal_transition_with_action<std::remove_cvref_t<Transition>, std::remove_cvref_t<Action>>;
+
+public:
+    template<typename Event, typename Destination>
+    [[nodiscard]] static constexpr transition auto create_transition() noexcept
+    {
+        return internal_transition<std::remove_cvref_t<Event>, std::remove_cvref_t<Destination>>{};
+    }
+
+    template<typename Destination>
+    [[nodiscard]] static constexpr transition auto create_default_transition() noexcept
+    {
+        return create_transition<default_event_t, Destination>();
+    }
+
+    template<transition Transition, detail::transition_guard<detail::event_t<Transition>> Guard>
+    requires (!requires{ typename detail::guard_t<Transition>; })
+    [[nodiscard]] friend constexpr transition_with_guard auto operator|(Transition&& t, Guard&& g) noexcept(noexcept(internal_transition_with_guard{std::declval<Transition>(), std::declval<Guard>()}))
+    {
+        return internal_transition_with_guard{std::forward<Transition>(t), std::forward<Guard>(g)};
+    }
+
+    template<transition Transition, detail::transition_action<detail::event_t<Transition>> Action>
+    requires (!requires{ typename detail::action_t<Transition>; })
+    [[nodiscard]] friend constexpr transition_with_action auto operator|(Transition&& t, Action&& a) noexcept(noexcept(internal_transition_with_action{std::declval<Transition>(), std::declval<Action>()}))
+    {
+        return internal_transition_with_action{std::forward<Transition>(t), std::forward<Action>(a)};
+    }
 };
 
 }
